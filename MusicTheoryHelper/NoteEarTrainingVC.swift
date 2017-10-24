0e4423332e8818af5ea42422a9769944c32b13d9
Chad Hamdan <chad.hamdan4@gmail.com>
2017-10-24 11:04:57 -0400

    Added Note Ear Training Interface
    - Also fixed crashing when cells were selected at main menu

---
+++ MusicTheoryHelper/NoteEarTrainingVC.swift
@@ -0,0 +1,13 @@
+//
+//  NoteEarTrainingVC.swift
+//  MusicTheoryHelper
+//
+//  Created by Chad Hamdan on 10/17/17.
+//  Copyright © 2017 Chad Hamdan. All rights reserved.
+//
+
+import UIKit
+
+class NoteEarTrainingVC: UIViewController {
+    
+}
